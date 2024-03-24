35a11f9edec8c132d4b282dfbabbd05630f527df
Hewie.Sung <SH9830@outlook.com>
2024-03-24 19:34:27 +0800

    feat: 任务调度&fiber架构

---
+++ vite-runner/main.js
@@ -0,0 +1,4 @@
+import ReactDOM from "./core/ReactDom.js";
+import App from './App.js';
+// render(App, document.querySelector('#root'))
+ReactDOM.createRoot(document.querySelector('#root')).render(App)
